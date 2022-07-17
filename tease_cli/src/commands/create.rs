aa25b7f977bd7e5e96c23210aabe18bc815b6fcc
begnoo <zoranjankov99@gmail.com>
2022-07-17 21:15:59 +0200

    feat: added cli arguments and repo creation

---
+++ tease_cli/src/commands/create.rs
@@ -0,0 +1,35 @@
+use std::io::Write;
+use std::fs;
+use std::fs::File;
+use std::path::Path;
+
+
+pub fn create_repo(repo_name: String) -> String {
+
+    let path_buff = Path::new(&repo_name).join(".tease");
+    if path_buff.as_path().exists() {
+        return format!("Folder with name {:?} already exists.", repo_name).to_string();
+    }
+
+    return match fs::create_dir_all(path_buff.as_path()) {
+        Ok(_folder) => {
+
+            create_tease_file(Path::new(&repo_name).join(".tease").join("log").as_path(), "# Commit log")
+                .expect(&format!("Couldn't create log file for repo {:?}", repo_name).to_string());
+
+            create_tease_file(Path::new(&repo_name).join(".tease").join("temp").as_path(), "# Temp commit")
+                .expect(&format!("Couldn't create log file for repo {:?}", repo_name).to_string());
+
+            format!("tease cli successfully created a new repo named {:?}", repo_name).to_string()
+        },
+        Err(error) => format!("Problem creating the folder: {0}", error.to_string()).to_string(),
+    };
+
+}
+
+
+fn create_tease_file(path: &Path, message: &str) -> std::io::Result<()> {
+    let mut file = File::create(path)?;
+    file.write_all(message.as_bytes())?;
+    Ok(())
+}
---
+++ tease_cli/src/main.rs
@@ -0,0 +1,62 @@
+mod commands;
+
+use crate::commands::create::create_repo;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Direct command such as create, clone, commit, push 
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    
+    /// Create new tease repo
+    Create {
+        /// New repo name, if left empty it will be tease_repo.
+        repo_name: Option<String> 
+    },
+
+    /// Add file changes to commit 
+    Add {
+        /// Path to file or folder which you want to add to the commit, if left empty all changes will be added.
+        file_path: Option<String>
+    },
+    
+    /// Commit added file changes to repo
+    Commit { 
+        /// Commit message for added changes
+        message: Option<String> 
+    },
+}
+
+fn main() {
+
+    let args = Args::parse();
+
+    match &args.command {
+        Some(Commands::Create { repo_name }) => {
+            let deref_repo_name = repo_name.as_ref().map_or("tease_repo", |repo_name| repo_name);
+            println!("tease cli trying to create {:?}...", deref_repo_name);            
+            let result = create_repo(deref_repo_name.to_string());
+            println!("{0}", result);
+        }
+        
+        Some(Commands::Add { file_path }) => {
+            println!("tease cli trying to add {:?}...", file_path.as_ref().map_or(".", |file_path| file_path));
+        }
+        
+        Some(Commands::Commit { message }) =>  {
+            let commit_message =  message.as_ref().map_or("default", |message| message);
+            println!("tease cli trying to commit {:?}...", commit_message);
+        }
+
+        None => {
+            println!("type tease --help for info.");
+        }
+    }
+
+}
